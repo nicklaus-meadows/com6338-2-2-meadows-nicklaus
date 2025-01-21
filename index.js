ed65d72cb53fce6d1bcf4b12a22e7446efad6196
Nicklaus <nicklaus.meadows@ufl.edu>
2025-01-21 15:50:59 -0500

    setup rough test for confirm option testing

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function greet() {
-    let name = prompt("What is your name?")
-    if (name) {
-        alert('Hello, ' + name)
+    let user = prompt("What is your name?")
+    if (user) {
+        alert('Hello, ' + user)
+    } else {
+        alert('Please provide your name next time...')
     }
+    
+        let age = prompt("How old are you?")
+        if (age) {
+            console.log(parseInt(age))
+        } else {
+            alert('Please tell us your age.')
+        }
+        
+            let birthdayYet = confirm('You have had a birthday this year already?')
+            if (birthdayYet) {
+                console.log("hit ok")
+            } else {
+                console.log('hit cancel')
+            }
+
 }
     greet()
+
+    
